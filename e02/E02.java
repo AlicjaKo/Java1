0a37f64ed491a0f263b5eb3159c3f7130971820c
Ala <ala@Alas-MacBook-Pro.local>
2024-01-13 22:00:11 +0200

    did exercise

--- e02/E02.java
+++ e02/E02.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 class E02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number: ");
+        System.out.println("Give a number: ");
         int x = scanner.nextInt();
 
-        System.out.println("Enter a number: ");
+        System.out.println("Give a number: ");
         int y = scanner.nextInt();
 
         System.out.println(x+" + " +y+" = "+(x+y));

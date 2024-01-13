41e487e86a0e09a3e05bcdc6c9c3883724d53094
Ala <ala@Alas-MacBook-Pro.local>
2024-01-13 22:04:32 +0200

    did exercise

--- e03/E03.java
+++ e03/E03.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 class E03 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int x = scanner.nextInt();
+        if (x>0) {
+            System.out.println("Positive number");
+        }
+        else {
+            System.out.println("Negative number");
+        }
     }
 }

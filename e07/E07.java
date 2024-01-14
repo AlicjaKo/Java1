c611e022d9246680d29896cd3b8771d60122e7b3
Ala <ala@Alas-MacBook-Pro.local>
2024-01-14 14:41:44 +0200

    did exercise

--- e07/E07.java
+++ e07/E07.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 class E07 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which multiplicand multiplication table to show?");
+        int x = scanner.nextInt();
+        System.out.println("Until which multiplyer?");
+        int y = scanner.nextInt();
+        for (int i = 1; i<y+1; i++) {
+            System.out.println(i + " x " + x + " = " + (x*i));
+        }       
     }
 }

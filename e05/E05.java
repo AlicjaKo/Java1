11b60e729cbbaee262f4a6bb096a1fd2d4e4fa33
Ala <ala@Alas-MacBook-Pro.local>
2024-01-14 14:32:30 +0200

    did exercise

--- e05/E05.java
+++ e05/E05.java
@@ -19,7 +19,7 @@ class E05 {
                     System.out.println("Thursday");
                     break;
                 case 5:
-                    System.out.println("Friday");
+                    System.out.println("Fri");
                     break;
                 case 6:
                     System.out.println("Saturday");

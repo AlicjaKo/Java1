bf65d127d83d6af9365170d383ec7eac5aa3738d
Ala <ala@Alas-MacBook-Pro.local>
2024-01-14 14:43:21 +0200

    did exercise

--- e07/E07.java
+++ e07/E07.java
@@ -7,7 +7,7 @@ class E07 {
         int x = scanner.nextInt();
         System.out.println("Until which multiplyer?");
         int y = scanner.nextInt();
-        for (int i = 1; i<y+1; i++) {
+        for (int i = 0; i<y+1; i++) {
             System.out.println(i + " x " + x + " = " + (x*i));
         }       
     }

30d9aca24f7b5cf7cb46b3cfc9c1305a630947e9
Ala <ala@Alas-MacBook-Pro.local>
2024-01-14 14:34:09 +0200

    did exercise

--- e05/E05.java
+++ e05/E05.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class E05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number: ");
+        System.out.println("Enter a number:");
         int x = scanner.nextInt();
         if (x>0 && x<8) {
             switch (x) {
